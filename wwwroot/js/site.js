272ff9e667e4411ffcd85ea7cea0bab529f9da0c
SrbastianM <sebas907.l@gmail.com>
2022-03-01 10:25:37 -0500

    Realizados los ajustes para el uso de .net Web app

---
+++ wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+const nameValidation = getElementById(email_address);
+
+function validateInformationOfTheUser() {
+    if(nameValidation == 'pruebas') {
+        console.log('True')
+    } else {
+        console.error('incorrect User')
+    }
+}
